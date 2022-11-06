ad36e465ffd2692c43540cdae475df45124ea295
tomikaka22 <szabotamastibor@tutanota.com>
2022-11-06 12:07:10 +0100

    a11y onclick warning kikapcsolva

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,13 @@ import preprocessor from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocessor(),
+
+	onwarn: (warning, handler) => {
+		// disable a11y onclick warning
+		if (warning.code.startsWith("a11y-click-events-have-key-events")) return;
+		handler(warning);
+  	},
+
 	kit: {
 		adapter: adapter({ out: 'bufe-app' })
 	}

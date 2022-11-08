cea93b734e333742436243cebe893e658bee0212
tomikaka22 <szabotamastibor@tutanota.com>
2022-11-08 22:53:48 +0100

    Jobb rendelés kezelés

--- src/routes/api/vasarlas/+server.js
+++ src/routes/api/vasarlas/+server.js
@@ -1,7 +1,10 @@
 import { json } from "@sveltejs/kit";
 
 let rendelesek = {};
+let kesz = {};
 let counter = 1000;
+
+
 
 export async function POST({ request }) {
    let orderID = counter--;
@@ -14,17 +17,35 @@ export async function POST({ request }) {
    })
 };
 
+
+// get data
 export async function GET() {
    return json({
-      rendelesek: rendelesek
+      rendelesek: rendelesek,
+      kesz: kesz
    })
 };
 
-export async function DELETE({ request }) {
+
+// ready
+export async function PUT({ request }) {
    let adat = await request.json();
+   kesz[adat] = rendelesek[adat]
    delete rendelesek[adat];
    return json({
       ok : 'ok'
    })
 };
 
+export async function DELETE({ request }) {
+   let adat = await request.json();
+   delete kesz[adat];
+   if (adat == "debugDelete") {
+      kesz = {};
+      rendelesek = {}
+   };
+
+   return json({
+      ok : 'ok'
+   })
+}

36aab1d415d4835dd0cd1a868fb5594aef0fe827
tomikaka22 <szabotamastibor@tutanota.com>
2022-12-07 19:21:23 +0100

    pwa implementacio szuneteltetese

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			scope: '/',
+			manifest: {
+				'name': 'bufe-app',
+				'display': 'standalone',
+				'id': 'bufe-app',
+				'start_url': '/',
+				'background_color': 'black',
+				'icons': [
+					{
+						'src': 'pwa-icon.svg',
+						'sizes': 'any'
+					}
+				],
+				'description': 'Rendelj büféből a mobilodon, vedd át szünetben sorbanállás nélkül.'
+			},			
+			devOptions: {
+				enabled: true,
+				type: 'module',
+				navigateFallback: '/'
+			}
+		})
+	]
 };
 
 export default config;

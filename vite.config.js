099d2b8146e90169eb0f82ea1dd31ece8c676297
tomikaka22 <szabotamastibor@tutanota.com>
2022-12-15 20:45:08 +0100

    frissites

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 const config = {
 	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			registerType: 'autoUpdate',
-			scope: '/',
-			manifest: {
-				'name': 'bufe-app',
-				'display': 'standalone',
-				'id': 'bufe-app',
-				'start_url': '/',
-				'background_color': 'black',
-				'icons': [
-					{
-						'src': 'pwa-icon.svg',
-						'sizes': 'any'
-					}
-				],
-				'description': 'Rendelj büféből a mobilodon, vedd át szünetben sorbanállás nélkül.'
-			},			
-			devOptions: {
-				enabled: true,
-				type: 'module',
-				navigateFallback: '/'
-			}
-		})
+		sveltekit()
 	]
 };
 

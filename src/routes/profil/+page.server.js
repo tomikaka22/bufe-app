0b09c9c73f89315b78c0ad3784eb267e5d3a67e0
tomikaka22 <szabotamastibor@tutanota.com>
2022-12-16 18:47:43 +0100

    cleanup

--- src/routes/profil/+page.server.js
+++ src/routes/profil/+page.server.js
@@ -5,10 +5,6 @@ export const actions = {
 	changeAvatar: async ({ request, locals }) => {
 		const data = await request.formData();
 		locals.pb.collection('users').update(locals.pb.authStore.baseModel.id, data);
-
-		return {
-			success: true
-		};
 	},
 	changePassword: async ({ request, locals }) => {
 		

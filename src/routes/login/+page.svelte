7bcde28a3386cfa4e48db938a3f4f2d77e80ca19
tomikaka22 <szabotamastibor@tutanota.com>
2023-02-06 20:24:44 +0100

    asdasd

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -10,20 +10,20 @@
    
    <main in:fade={{duration: 180}} class="font-sofia bg-gray-2 w-screen h-screen">
       <div class="login flex flex-col items-center pt-24">
-         <h1 class="font-extrablack text-5xl text-red-1">Kandó Büfé</h1>
-         <h2 class="text-red-1 text-2xl font-bold mt-12 mb-6">Bejelentkezés</h2>
+         <h1 class="font-extrablack text-5xl">Kandó Büfé</h1>
+         <h2 class="text-2xl font-bold mt-12 mb-6">Bejelentkezés</h2>
    
          <form method="post" class="flex flex-col items-center">
             <div class="flex flex-row mb-6">
-               <input placeholder="Kandós e-mail" name='email' bind:value={email} id="email-form" type="text" class="border-gray bg-gray-1 p-2 rounded-md" />
+               <input placeholder="Kandós e-mail" name='email' bind:value={email} id="email-form" type="text" class="border-gray p-2 rounded-md shadow-xl" />
             </div>
             <div class="flex flex-row mb-6">
                <label for="password-form"></label><br />
-               <input placeholder="Jelszó" name='password' bind:value={password} id="password-form" type="password" class="border-gray bg-gray-1 p-2 rounded-md" />
+               <input placeholder="Jelszó" name='password' bind:value={password} id="password-form" type="password" class="border-gray p-2 rounded-md shadow-xl" />
             </div>
             <div class="flex flex-col justify-center">
                {#if email && password}
-                  <button in:fade={{duration: 300}} class="bg-red-1 text-white text-lg px-8 py-2 rounded-md font-semibold">Belépés</button>
+                  <button in:fade={{duration: 300}} class=" text-lg px-8 py-2 rounded-md font-semibold">Belépés</button>
                {/if}
             </div>
             {#if error && !email || !password && error != undefined}
@@ -33,7 +33,7 @@
    
          <div class="regin-button flex flex-col items-center">
             <h2 class="mt-3 mb-1">Nincs még fiókod?</h2>
-            <a href="/register"><button class="bg-gray-1 text-red-1 text-lg px-8 py-2 rounded-md font-semibold">Regisztráció</button></a>
+            <a href="/register"><button class="text-lg px-8 py-2 rounded-md font-semibold shadow-xl outline">Regisztráció</button></a>
          </div>
    
       </div>

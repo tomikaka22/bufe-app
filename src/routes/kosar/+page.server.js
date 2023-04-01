31c59fcc91995befa95b2e0b1118c9e9e43119c1
tomikaka22 <szabotamastibor@tutanota.com>
2023-04-01 11:07:39 +0200

    Időpont, fizetési mód validálás.

--- src/routes/kosar/+page.server.js
+++ src/routes/kosar/+page.server.js
@@ -2,35 +2,34 @@ import { fail } from '@sveltejs/kit';
 
 export const ssr = false;
 
-let szunetArray = [];
-
 function timeToMs(time) {
 	// return Date.parse(new Date(new Date().setHours(...time.split(':'))).toLocaleString('hu-HU', { timeZone: 'Europe/Budapest' }));	// Unix time
 	const splitTime = time.split(':');
 	return splitTime[0] * 3600000 + splitTime[1] * 60000 + splitTime[2] * 1000;
 }
 
-export async function load({ locals }) {
-	const most = timeToMs(new Date().toLocaleTimeString('hu-HU', { timeZone: 'Europe/Budapest' }));
-
-	if (most < timeToMs('28:00:00'))
-		szunetArray = [ '1. Óra előtt','2. Óra előtti szünet','3. Óra előtti szünet','4. Óra előtti szünet','5. Óra előtti szünet','6. Óra előtti szünet' ];
+function szunet(most = timeToMs(new Date().toLocaleTimeString('hu-HU', { timeZone: 'Europe/Budapest' }))) {
+	if (most < timeToMs('08:00:00'))
+		return [ '1. Óra előtt','2. Óra előtti szünet','3. Óra előtti szünet','4. Óra előtti szünet','5. Óra előtti szünet','6. Óra előtti szünet' ];
 	else if (most < timeToMs('08:55:00'))
-		szunetArray = [ '2. Óra előtti szünet','3. Óra előtti szünet','4. Óra előtti szünet','5. Óra előtti szünet','6. Óra előtti szünet' ];
+		return [ '2. Óra előtti szünet','3. Óra előtti szünet','4. Óra előtti szünet','5. Óra előtti szünet','6. Óra előtti szünet' ];
 	else if (most < timeToMs('09:50:00'))
-		szunetArray = [ '3. Óra előtti szünet','4. Óra előtti szünet','5. Óra előtti szünet','6. Óra előtti szünet' ];
+		return [ '3. Óra előtti szünet','4. Óra előtti szünet','5. Óra előtti szünet','6. Óra előtti szünet' ];
 	else if (most < timeToMs('10:45:00'))
-		szunetArray = [ '4. Óra előtti szünet','5. Óra előtti szünet','6. Óra előtti szünet' ];
+		return [ '4. Óra előtti szünet','5. Óra előtti szünet','6. Óra előtti szünet' ];
 	else if (most < timeToMs('11:40:00'))
-		szunetArray = [ '5. Óra előtti szünet','6. Óra előtti szünet' ];
+		return [ '5. Óra előtti szünet','6. Óra előtti szünet' ];
 	else if (most < timeToMs('12:45:00'))
-		szunetArray = [ '6. Óra előtti szünet' ];
+		return [ '6. Óra előtti szünet' ];
 	else
-		szunetArray = [];
+		return [];
+}
+
+export async function load({ locals }) {
 
 	return {
 		'termekek': structuredClone(await locals.pb.collection('termekek').getFullList(1, {})),
-		szunetArray
+		'szunetArray': szunet()
 	};
 }
 
@@ -39,8 +38,6 @@ export const actions = {
 	default: async ({ request, locals }) => {
 		const data = Object.fromEntries(await request.formData());
 		const rendeles = JSON.parse(data.rendeles);
-		const idopont = data.idopont;
-		const fizetes = data.fizetes;
 		let total = 0;
 
 		for (const termek in rendeles) {
@@ -53,14 +50,29 @@ export const actions = {
 			rendeles[termek] = { 'ar': subTotal, darab, feltet };
 			total += subTotal;
 
+			if (!szunet().includes(data.idopont)) {
+				return fail(409, { 'error': `Hibás időpont: ${data.idopont}` });	// Hibás időpont
+			}
+
+			if (![ 'Készpénz','Bankkártya' ].includes(data.fizetes)) {
+				return fail(409, { 'error': `Hibás fizetési mód: ${data.fizetes}` });	// Hibás fizetési mód
+			}
+
 			if (record.darab - darab >= 0) {
 				await locals.pb.collection('termekek').update(record.id, { 'darab': record.darab - darab } );	// darabszam kivonasa
 				await locals.pb.collection('termekek').update(record.id, { 'vasarlasok': record.vasarlasok + darab });	// termék vásárlás számláló + darab
-			}
-			else
-				return fail(409, { 'error': `Túl sok ${record.termek} a kosárban!`, 'sok': record.termek });
+			} else
+				return fail(409, { 'error': `Túl sok ${record.termek} a kosárban!`, 'sok': record.termek });	// A kosárban lévő termék elfogyott
 		}
 
-		await locals.pb.collection('rendelesek').create({ 'rendelo': locals.pb.authStore.baseModel.id, 'termekek': rendeles, total, 'status': 'fuggoben', 'name': locals.pb.authStore.baseModel.name, fizetes, idopont });
+		await locals.pb.collection('rendelesek').create({
+			'rendelo': locals.pb.authStore.baseModel.id,
+			'termekek': rendeles,
+			total,
+			'status': 'fuggoben',
+			'name': locals.pb.authStore.baseModel.name,
+			'fizetes': data.fizetes,
+			'idopont': data.idopont
+		});
 	}
 };

f029a9aa46784548b479abe1463eab534de86359
tomikaka22 <szabotamastibor@tutanota.com>
2023-04-10 22:08:56 +0200

    Legutóbbi termékek fix

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,14 +1,19 @@
 export async function load({ locals }) {
-	const elozmenyLista = structuredClone(await locals.pb.collection('rendelesek').getFullList(1, 6, {
+	const elozmenyLista = await locals.pb.collection('rendelesek').getList(1, 3, {
 		filter: `rendelo = "${locals.pb.authStore.baseModel.id}"`,
 		sort: '-created'
-	}));
+	});
 
-	const legutobbi6Termek = [ ...new Set(elozmenyLista.map(rendeles => { // Set --> kiszűri az ismétlődőket
-		return Object.keys(rendeles.termekek);
-	}).flat().splice(-7, 7).reverse()) ];
+	const legutobbi6Termek = [];
+	for (let i = 0; i < elozmenyLista.items.length; i++) {
+		const length = legutobbi6Termek.push(...Object.keys(elozmenyLista.items[i].termekek));
+		if (length > 2) {
+			legutobbi6Termek.slice(0, -length + 2);
+			break;
+		}
+	}
 
 	return {
-		legutobbi6Termek
+		'legutobbi6Termek' : [ ...new Set(legutobbi6Termek) ]	// Set --> kiszűri az ismétlődőket
 	};
 }

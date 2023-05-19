3cb30d13d3eeaf690ae265c247baa5ced5b9566f
Saul Goodman <113334790+gergogyulai@users.noreply.github.com>
2023-05-19 11:25:29 +0200

    better favs

--- src/routes/profil/+page.server.js
+++ src/routes/profil/+page.server.js
@@ -1,10 +1,10 @@
-let currentUser
+let currentUser;
 
 export async function load({ locals }) {
-	currentUser = structuredClone(await locals.pb.collection('users').getOne(locals.pb.authStore.baseModel.id));
+	currentUser = structuredClone(locals.pb.authStore.baseModel);
 
 	return {
-		currentUser,
+		currentUser
 	};
 }
 
@@ -24,26 +24,26 @@ export const actions = {
 		const newFavObj = JSON.parse(data.get('newFav'));
 		const keyToAdd = Object.keys(newFavObj)[0];
 
-		if (!keyToAdd) throw new Error("Invalid favorite data.");
+		if (!keyToAdd) throw new Error('Invalid favorite data.');
 
 		const dbfavs = currentUser.favs || {};
 		if (dbfavs.hasOwnProperty(keyToAdd)) return; // mar van ilyen kedvenc
 
 		dbfavs[keyToAdd] = newFavObj[keyToAdd];
-		await locals.pb.collection('users').update(locals.pb.authStore.baseModel.id, { "favs": dbfavs });
+		await locals.pb.collection('users').update(locals.pb.authStore.baseModel.id, { 'favs': dbfavs });
 
 	},
 	removeFav: async ({ request, locals }) => {
 		const data = await request.formData();
 		const favToRemove = data.get('removedFav');
 
-		if (!favToRemove) throw new Error("Invalid favorite data.");
+		if (!favToRemove) throw new Error('Invalid favorite data.');
 
 		const dbfavs = currentUser.favs || {};
-		if (!dbfavs.hasOwnProperty(favToRemove)) return; // nem torolheto mivel nincs ilyen kedvenc 
+		if (!dbfavs.hasOwnProperty(favToRemove)) return; // nem torolheto mivel nincs ilyen kedvenc
 
 		delete dbfavs[favToRemove];
-		await locals.pb.collection('users').update(locals.pb.authStore.baseModel.id, { "favs": dbfavs });
+		await locals.pb.collection('users').update(locals.pb.authStore.baseModel.id, { 'favs': dbfavs });
 
 	}
 };

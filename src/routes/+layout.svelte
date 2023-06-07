b10ebaa48aed9539c357f41d5820e9045c1e8394
tomikaka22 <szabotamastibor@tutanota.com>
2023-06-07 22:01:19 +0200

    Workbox remove, npm update.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,6 @@
 <script>
 	import PageTransition from '$lib/components/PageTransition.svelte';
-	import {
-		page
-	} from '$app/stores';
-	import {
-		onMount
-	} from 'svelte';
-	import {
-		pwaInfo
-	} from 'virtual:pwa-info';
+	import { page } from '$app/stores';
 	import '../app.css';
 
 	const noKeyURLs = [ '/admin', '/rendelesek' ];
@@ -38,30 +30,6 @@
 		'Húszezresből mennyi van?',
 		'““”̿ ̿ ̿ ̿ ̿’̿’̵͇̿̿з=(*‿*)=ε/̵͇̿̿/̿ ̿ ̿ ̿ ̿’““'
 	];
-
-	onMount(async () => {
-		if (pwaInfo) {
-			const {
-				registerSW
-			} = await import('virtual:pwa-register');
-			registerSW({
-				immediate: true,
-				onRegistered(r) {
-				// uncomment following code if you want check for updates
-				// r && setInterval(() => {
-				//    console.log('Checking for sw update')
-				//    r.update()
-				// }, 20000 /* 20s for testing purposes */)
-					console.log(`SW Registered: ${r}`);
-				},
-				onRegisterError(error) {
-					console.log('SW registration error', error);
-				}
-			});
-		}
-	});
-
-  $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
 
 {#if !noKeyURLs.includes($page.url.pathname)}
@@ -74,7 +42,6 @@
 
 <svelte:head>
 	<title>{splash[Math.floor(Math.random() * splash.length)]}</title>
-    {@html webManifest}
 </svelte:head>
 
 <style>

4f989fb9af65e3b4d2c674f0c64d9ecd99d72183
tomikaka22 <szabotamastibor@tutanota.com>
2023-07-31 17:00:27 +0200

    SameSite=lax

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -28,7 +28,7 @@ export async function handle({ event, resolve }) {
 	const response = await resolve(event);
 
 	// send back the default 'pb_auth' cookie to the client with the latest store state
-	response.headers.append('set-cookie', event.locals.pb.authStore.exportToCookie());
+	response.headers.append('set-cookie', event.locals.pb.authStore.exportToCookie({ sameSite: 'lax' }));
 
 	return response;
 }

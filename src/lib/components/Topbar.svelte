c6c42eb86f64defec8d0449657b5b8f8b748c567
tomikaka22 <szabotamastibor@tutanota.com>
2023-08-16 23:33:05 +0200

    topbar adjustments 2

--- src/lib/components/Topbar.svelte
+++ src/lib/components/Topbar.svelte
@@ -9,9 +9,9 @@
 
 </script>
 
-<div in:fly={flyin} class="w-full flex items-center text-primary p-2 pl-1 mb-2">
-	<a class="pr-1" href="{targeturl}">
-		<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="48"><path d="m274-450 248 248-42 42-320-320 320-320 42 42-248 248h526v60H274Z"/></svg>
+<div in:fly={flyin} class="w-full flex items-center text-primary p-2 pl-1 mt-1 mb-2">
+	<a class="pr-1.5" href="{targeturl}">
+		<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="23" viewBox="0 -960 960 960" width="48"><path d="m283.805-445.935 244.173 244.174L480-154.022 154.022-480 480-806.218l47.978 47.979-244.173 244.174h522.413v68.13H283.805Z"/></svg>
 	</a>
 	<p class="text-lg">{text}</p>
 </div>

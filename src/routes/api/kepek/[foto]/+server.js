c183a6cf17936a932dc034163fa46c626d36c384
tomikaka22 <szabotamastibor@tutanota.com>
2023-08-20 01:18:52 +0200

    Revert "Termék képek api tweak."
    This reverts commit d1c7574dec8984f35f5df10a98f2a282a1e4d237.

--- src/routes/api/kepek/[foto]/+server.js
+++ src/routes/api/kepek/[foto]/+server.js
@@ -3,6 +3,7 @@ export async function GET({ locals, fetch, url }) {
 	const record = await locals.pb.collection('termekek').getOne(id);
 
 	let foto = await fetch(`http://127.0.0.1:8090/api/files/termekek/${id}/${record.foto}`);
+	console.log('foto requested', id)
 
 	if (foto.status === 404)
 		foto = await fetch(`https://ui-avatars.com/api/?size=256&name=${locals.pb.authStore.baseModel.name}&color=ebebec&background=252525`);

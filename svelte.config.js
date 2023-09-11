4f5c54d473b4f41c4b6aa31fb9c52eaf7f6f0ed1
tomikaka22 <szabotamastibor@tutanota.com>
2023-09-11 18:48:35 +0200

    bun test 2

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 	},
 
 	kit: {
-		adapter: adapter({ polyfill: false }),
+		adapter: adapter({ dynamic_origin: true }),
 		serviceWorker: { register: false },
 		version: { name: process.env.npm_package_version + ' ' + (new Date).toLocaleString('hu-HU').split('. ').join('').split(':').join('') }
 	}

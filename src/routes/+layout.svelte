e8078a1691b3dc3a0cd3820a566724988f68341d
tomikaka22 <szabotamastibor@tutanota.com>
2023-10-13 20:11:34 +0200

    Kosár badge fix.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -115,8 +115,8 @@
 			</div>
 		</a>
 
-		<a href="/kosar" class="flex flex-col justify-center items-center h-16 w-full transition-all" class:active-nav={kosarRoutes.includes($page.route.id)}>
-			<div class="w-1/2 h-1/2 relative flex justify-center items-center rounded-3xl py-1" class:active-img={kosarRoutes.includes($page.route.id)} style="transition-timing-function: cubic-bezier(0.05, 0.7, 0.1, 1.0);">
+		<a href="/kosar" class="flex flex-col justify-center items-center h-16 w-full transition-all relative" class:active-nav={kosarRoutes.includes($page.route.id)}>
+			<div class="w-1/2 h-1/2 flex justify-center items-center rounded-3xl py-1" class:active-img={kosarRoutes.includes($page.route.id)} style="transition-timing-function: cubic-bezier(0.05, 0.7, 0.1, 1.0);">
 				{#if kosarRoutes.includes($page.route.id)}
 					<svg class="h-full w-full text-primary" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M286.788-81Q257-81 236-102.212q-21-21.213-21-51Q215-183 236.212-204q21.213-21 51-21Q317-225 338-203.788q21 21.213 21 51Q359-123 337.788-102q-21.213 21-51 21Zm400 0Q657-81 636-102.212q-21-21.213-21-51Q615-183 636.212-204q21.213-21 51-21Q717-225 738-203.788q21 21.213 21 51Q759-123 737.788-102q-21.213 21-51 21ZM205-801h589.074q22.964 0 34.945 21Q841-759 829-738L694-495q-11 19-28.559 30.5Q647.881-453 627-453H324l-56 104h491v60H277q-42 0-60.5-28t.5-63l64-118-152-322H51v-60h117l37 79Z"/></svg>
 				{:else}
